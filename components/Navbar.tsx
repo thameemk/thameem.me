f87f7e43d40acf5088135ab203c7ae0152523aff
thameemk612 <thameemk612@gmail.com>
2022-11-27 20:19:14 +0530

    Add: Mobile Menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import cn from "classnames";
 import NextLink from 'next/link';
 import {useRouter} from "next/router";
+import MobileMenu from "./MobileMenu";
 
 
 function NavItem({href, text}: { href: any, text: string }) {
@@ -30,6 +31,7 @@ function Navbar() {
                 className="flex items-center justify-between w-full relative max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16  text-gray-900 bg-gray-50  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
 
                 <div className="ml-[-0.60rem]">
+                    <MobileMenu/>
                     <NavItem href="/" text="Home"/>
                     <NavItem href="/" text="About"/>
                     <NavItem href="/" text="Blog"/>
