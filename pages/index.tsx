d3ce36d8441a002fb8ecf1a1dd0aec09d49802f7
thameemk612 <thameemk612@gmail.com>
2022-12-14 23:37:12 +0530

    Home Contents

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,9 @@ import React from "react";
 export default function Home() {
     return (
         <Container>
-            <p>Hello</p>
+            <h1>Thameem Karakkoth</h1>
+            <h6>Product Engineer (Backend) at <b>UST</b></h6>
+            <p>Developer with a keen interest in cyber security</p>
         </Container>
     )
 }
