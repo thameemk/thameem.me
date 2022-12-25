510325e32a5bd29c319809672d6917b27bc90c9b
thameemk612 <thameemk612@gmail.com>
2022-12-25 23:58:58 +0530

    change: desktop menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,9 +13,9 @@ function NavItem({href, text}: { href: any, text: string }) {
             href={href}
             className={cn(
                 isActive
-                    ? 'font-semibold text-gray-800 dark:text-gray-200'
-                    : 'font-normal text-gray-600 dark:text-gray-400',
-                'hidden md:inline-block p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-all'
+                    ? 'font-semibold text-gray-200'
+                    : 'font-normal text-gray-400',
+                'hidden md:inline-block p-1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-800  transition-all'
             )}
         >
             <span className="capsize">{text}</span>
@@ -28,7 +28,7 @@ function Navbar() {
     return (
         <div className="flex flex-col justify-center px-8">
             <nav
-                className="flex items-center justify-between w-full relative max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16  text-gray-900 bg-gray-50  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
+                className="flex items-center justify-between w-full relative max-w-2xl mx-auto pt-8 pb-8 sm:pb-16  ">
 
                 <div className="ml-[-0.60rem]">
                     <MobileMenu/>
