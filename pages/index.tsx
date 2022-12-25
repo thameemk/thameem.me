e164a198610747e78d2b04a3f237eea139756340
thameemk612 <thameemk612@gmail.com>
2022-12-26 00:28:13 +0530

    add: intro - home

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,22 @@ import React from "react";
 export default function Home() {
     return (
         <Container>
+            <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
+                <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-white">
+                    Thameem Karakkoth
+                </h1>
+                <h2 className="text-gray-200 mb-4">
+                    Product Engineer at{' '}
+                    <span className="font-semibold">UST</span>
+                </h2>
+                <div className="mb-8">
+                    <p className="text-gray-400 mb-4 text-justify">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since the 1500s,Lorem Ipsum is simply dummy text of the
+                        printing and typesetting industry. Lorem Ipsum has been the
+                    </p>
+                </div>
+            </div>
         </Container>
     )
 }
