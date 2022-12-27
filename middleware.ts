e35c368fe5c7123dff6cf48a80ea724d3429d1c1
thameemk612 <thameemk612@gmail.com>
2022-12-27 22:01:29 +0530

    add : auth

---
+++ middleware.ts
@@ -0,0 +1,29 @@
+/*
+ * Project : thameem.me
+ * Filename : middleware.ts
+ * Author : thameem
+ * Modified time : Tue, 27 Dec 2022 at 9:41 pm India Standard Time
+ */
+
+import {NextRequest, NextResponse} from 'next/server'
+
+export const config = {
+    matcher: ['/', '/index'],
+}
+
+export function middleware(req: NextRequest) {
+    const basicAuth = req.headers.get('authorization')
+    const url = req.nextUrl
+
+    if (basicAuth) {
+        const authValue = basicAuth.split(' ')[1]
+        const [user, pwd] = atob(authValue).split(':')
+
+        if (user === 'admin' && pwd === 'admin@123') {
+            return NextResponse.next()
+        }
+    }
+    url.pathname = '/api/auth'
+
+    return NextResponse.rewrite(url)
+}
