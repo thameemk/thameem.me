f7e4543f349239339682b35b39f1762c5e6ea994
thameemk612 <thameemk612@gmail.com>
2022-12-27 21:35:43 +0530

    change: blog card - background style

--- components/BlogPostCard.tsx
+++ components/BlogPostCard.tsx
@@ -8,13 +8,16 @@ export default function BlogPostCard() {
         <Link
             href={'/'}
         >
-            <div className="bg-[url('/static/images/blog/sample_blog.jpg')]
-            rounded-2xl h-64 w-48"
-            >
-                <h4>
-                    sample title
-                </h4>
-            </div>
+            <article
+                className="rounded-2xl h-64 mx-auto max-w-sm shadow-xl bg-cover bg-center min-h-150 transform duration-500 hover:-translate-y-2 cursor-pointer group"
+                style={{backgroundImage: 'url(/static/images/blog/sample_blog.jpg)'}}>
+                <div
+                    className="bg-black bg-opacity-20 px-10 flex flex-wrap flex-col pt-48 hover:bg-opacity-75 transform duration-300">
+                    <h4 className="text-white">
+                        On A Day Like Today
+                    </h4>
+                </div>
+            </article>
 
         </Link>
     );
