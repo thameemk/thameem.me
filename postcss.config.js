caed5bf0535400e8da3ed9bafaf02d39fac003b4
thameemk612 <thameemk612@gmail.com>
2023-01-16 21:28:01 +0530

    change style - tailwindcss

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Project : thameem.me
+ * Filename : postcss.config.js
+ * Author : thameem
+ * Modified time : Mon, 16 Jan 2023 at 8:24 pm India Standard Time
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Project : thameem.me
+ * Filename : postcss.config.js
+ * Author : thameem
+ * Modified time : Sun, 27 Nov 2022 at 7:40 pm India Standard Time
+ ******************************************************************************/
+
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+    },
+}
