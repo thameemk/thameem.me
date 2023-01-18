b654b9f22dd112969e8186e4fbc088f3bb4eae13
thameemk612 <thameemk612@gmail.com>
2023-01-18 23:01:38 +0530

    add: coming soon component

---
+++ components/ComingSoon.tsx
@@ -0,0 +1,17 @@
+/*
+ * Project : thameem.me
+ * Filename : ComingSoon.ts
+ * Author : thameem
+ * Modified time : Wed, 18 Jan 2023 at 10:59 pm India Standard Time
+ */
+
+
+function ComingSoon() {
+    return (
+        <div className="margin-c text-center font-black">
+            Coming Soon...😇
+        </div>
+    )
+}
+
+export default ComingSoon
