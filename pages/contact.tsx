b76a0139b3722c993efc971fe4dded4bd4864a64
thameemk612 <thameemk612@gmail.com>
2023-01-18 23:04:04 +0530

    add: contact coming soon page

---
+++ pages/contact.tsx
@@ -0,0 +1,20 @@
+/*
+ * Project : thameem.me
+ * Filename : contact.ts
+ * Author : thameem
+ * Modified time : Wed, 18 Jan 2023 at 11:01 pm India Standard Time
+ */
+
+
+import Container from "../components/Container";
+import ComingSoon from "../components/ComingSoon";
+
+function Contact() {
+    return (
+        <Container title={"Contact - Thameem Karakkoth"}>
+            <ComingSoon/>
+        </Container>
+    )
+}
+
+export default Contact
