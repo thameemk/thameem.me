2e9c25265fbc4deecc18f1be0c027485ef625a68
thameemk612 <thameemk612@gmail.com>
2023-01-19 21:17:36 +0530

    add: blog layout sample

---
+++ layouts/blog.tsx
@@ -0,0 +1,20 @@
+/*
+ * Project : thameem.me
+ * Filename : blog.tsx
+ * Author : thameem
+ * Modified time : Thu, 19 Jan 2023 at 8:48 pm India Standard Time
+ */
+
+
+import Container from "../components/Container";
+import ComingSoon from "../components/ComingSoon";
+
+function Blog() {
+    return (
+        <Container>
+            <ComingSoon/>
+        </Container>
+    )
+}
+
+export default Blog
