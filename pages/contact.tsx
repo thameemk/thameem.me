f5f35525a133217b8fbc5d72049a72e85a93a3b7
thameemk612 <thameemk612@gmail.com>
2023-01-20 22:52:10 +0530

    change: contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -12,26 +12,18 @@ function Contact() {
     return (
         <Container title={"Contact - Thameem Karakkoth"}>
             <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">share your
-                    thoughts</h2>
-                <p className="mb-8 lg:mb-16 font-light text-center text-gray-500  sm:text-xl">Got
-                    a technical issue? Want to send feedback about a beta feature? Need details about our Business
-                    plan? Let us know.</p>
+                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Get in touch</h2>
+                <p className="mb-8 lg:mb-16 font-light text-center text-gray-500  sm:text-xl">
+                    share your thoughts anonymously
+                </p>
                 <form action="#" className="space-y-8">
-                    <div>
-                        <label htmlFor="email"
-                               className="block mb-2 text-sm font-medium text-gray-900">Your
-                            email</label>
-                        <input type="email" id="email"
-                               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
-                               placeholder="name@flowbite.com" required/>
-                    </div>
+
                     <div>
                         <label htmlFor="subject"
                                className="block mb-2 text-sm font-medium text-gray-900">Subject</label>
                         <input type="text" id="subject"
                                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500"
-                               placeholder="Let us know how we can help you" required/>
+                               placeholder="Let us know how I can help you" required/>
                     </div>
                     <div className="sm:col-span-2">
                         <label htmlFor="message"
