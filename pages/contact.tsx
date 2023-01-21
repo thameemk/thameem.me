0c6c2d7e69d377f329554df80fb964ecf1043f59
thameemk612 <thameemk612@gmail.com>
2023-01-21 23:05:35 +0530

    connect contact form to google sheet

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -7,24 +7,29 @@
 
 
 import Container from "../components/Container";
-import React from "react";
+import React, {useRef} from "react";
+
 
 function Contact() {
 
-    const saveToSheet = (event: React.FormEvent<HTMLFormElement>) => {
+    const formRef = useRef<any | null>(null)
+    const scriptUrl: any = process.env.GOOGLE_APP_SCRIPT_WEB_APP_URL;
+
+    const saveToSheet = (event: any) => {
+
         event.preventDefault();
 
-        const target = event.target as typeof event.target & {
-            subject: { value: string };
-            message: { value: string };
-        };
-        const subject = target.subject.value;
-        const message = target.message.value;
 
-        console.log(subject);
-        console.log(message);
+        fetch(scriptUrl, {
+            method: 'POST',
+            body: new FormData(formRef.current),
 
-        console.log("true")
+        }).then(res => {
+            console.log(res)
+            console.log("success")
+        })
+            .catch(err => console.log(err))
+
     }
 
     return (
@@ -34,11 +39,12 @@ function Contact() {
                 <p className="mb-8 lg:mb-16 font-light text-center text-gray-500  sm:text-xl">
                     share your thoughts anonymously
                 </p>
-                <form onSubmit={saveToSheet} className="space-y-8">
+                <form ref={formRef} onSubmit={saveToSheet} name="contact_form" className="space-y-8">
                     <div>
                         <label htmlFor="subject"
                                className="block mb-2 text-sm font-medium text-gray-900">Subject</label>
                         <input type="text" id="subject"
+                               name="subject"
                                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500"
                                placeholder="Let me know how I can help you" required/>
                     </div>
@@ -46,7 +52,9 @@ function Contact() {
                         <label htmlFor="message"
                                className="block mb-2 text-sm font-medium text-gray-900">Your
                             message</label>
-                        <textarea id="message" rows={6}
+                        <textarea id="message"
+                                  name="message"
+                                  rows={6}
                                   className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
                                   placeholder="Leave a comment..."></textarea>
                     </div>
