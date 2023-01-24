63ab97c0ed2a600b8e937facc3191f21c8bc67bc
thameemk612 <thameemk612@gmail.com>
2023-01-25 00:22:06 +0530

    fix: message field required

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -112,7 +112,7 @@ function Contact() {
                                   name="message"
                                   rows={6}
                                   className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
-                                  placeholder="Leave a comment..."></textarea>
+                                  placeholder="Leave a comment..." required></textarea>
                     </div>
                     <input type="submit"
                            value={loading ? "Sending..." : "Send message"}
