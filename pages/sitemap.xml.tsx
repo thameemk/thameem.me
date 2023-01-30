af57cd4fd857ea0ac8c46bced8bab4375d2d5dbc
thameemk612 <thameemk612@gmail.com>
2023-01-30 21:04:52 +0530

    change: dynamic sitemap generator

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -6,7 +6,7 @@
  */
 
 const createSitemap = (slugs: string[]) => `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
         ${slugs
     .map((slug) => {
         return `
