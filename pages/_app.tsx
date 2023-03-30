a8002edc4d48e537d15478d95ff8a7fd31137939
Thameem Karakkoth <thameemk612@gmail.com>
2023-03-30 14:50:17 +0530

    Change: Remove Google reCAPTCHA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,29 +9,18 @@ import '../styles/global.css'
 import type {AppProps} from 'next/app'
 import Script from "next/script";
 import React from "react";
-import {GoogleReCaptchaProvider} from "react-google-recaptcha-v3";
 
 export default function App({Component, pageProps}: AppProps) {
-    const recaptcha_key: any = process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_SITE_KEY
     return (
-        <GoogleReCaptchaProvider
-            reCaptchaKey={recaptcha_key}
-            scriptProps={{
-                async: false,
-                defer: false,
-                appendTo: "head",
-                nonce: undefined,
-            }}>
-            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-BFR3PPNSB4"></Script>
-            <Script id="google-analytics">
-                {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'G-BFR3PPNSB4');
-                `}
-            </Script>
-            <Component {...pageProps} />
-        </GoogleReCaptchaProvider>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-BFR3PPNSB4"></Script>
+        <Script id="google-analytics">
+            {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-BFR3PPNSB4');
+            `}
+        </Script>
+        <Component {...pageProps} />
     )
 }
