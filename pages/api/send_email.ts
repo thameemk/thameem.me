b221475ec4d95062e662f49f4ce97b5a14f48a39
thameemk <thameemk612@gmail.com>
2023-05-28 23:49:55 +0530

    Change: Google Recaptcha

--- pages/api/send_email.ts
+++ pages/api/send_email.ts
@@ -6,21 +6,8 @@
  */
 
 
-import type {NextApiRequest, NextApiResponse} from 'next'
-
-
-async function verifyGoogleRecaptcha(recaptchatoken: any) {
-
-    let response = await fetch('https://www.google.com/recaptcha/api/siteverify',
-        {
-            method: 'post',
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: `secret=${process.env.GOOGLE_RECAPTCHA_SECRET_KEY}&response=${recaptchatoken}`,
-        })
-    return await response.json()
-}
+import type {NextApiRequest, NextApiResponse} from 'next';
+import verifyGoogleRecaptcha from '../../libs/google_recaptcha';
 
 
 type Data = {
