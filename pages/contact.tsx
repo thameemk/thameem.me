ee890f0917357ddc813c3cc7a27cb4f55d82facb
thameemk <thameemk612@gmail.com>
2023-09-18 23:30:28 +0530

    Fix: Responsive [Contact Page - SM]

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -75,7 +75,7 @@ function Contact() {
 
   return (
     <Container title={"Contact - Thameem Karakkoth"}>
-      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+      <div className="py-8 lg:py-16 lg:px-4 mx-auto max-w-screen-md">
         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">
           Get in touch
         </h2>
