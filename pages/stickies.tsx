e15b41dbcf59a6e56e0cd88c399743db3e0ee307
thameemk <thameemk612@gmail.com>
2023-09-18 23:30:44 +0530

    Fix: Responsive [Stickies Page - SM]

--- pages/stickies.tsx
+++ pages/stickies.tsx
@@ -5,7 +5,7 @@ import ResearchPaper from "./stickies/ResearchPaper";
 function Stickies() {
   return (
     <Container title={"Stickies - Thameem Karakkoth"}>
-      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+      <div className="py-8 lg:py-16 lg:px-4 mx-auto max-w-screen-md">
         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">
           Random Things
         </h2>
